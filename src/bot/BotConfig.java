0d776d90a35cb0d94c8ebf62a0d55cfff4f26171
huyduc <huynguyen26nt@gmail.com>
2025-07-19 20:54:35 +0700

    v1

--- src/bot/BotConfig.java
+++ src/bot/BotConfig.java
@@ -3,8 +3,8 @@ package bot;
 
 public class BotConfig {
     public static final String SERVER_URL = "https://cf25-server.jsclub.dev";
-    public static final String GAME_ID = "166654";
-    public static final String PLAYER_NAME = "ThinhPham";
+    public static final String GAME_ID = "172116";
+    public static final String PLAYER_NAME = "huyduc";
     public static final String SECRET_KEY = "sk-XU28xPuqRli1j8ylC7W0Wg:Jvebs3-HuRPE9ia7ZQRicY3brKkozkLKciaK6t2j4zIb92n3kr20wckx-lbryR_uT9HPtvQukl0e2YAttU1obA";
     // Key Defeat: sk-XU28xPuqRli1j8ylC7W0Wg:Jvebs3-HuRPE9ia7ZQRicY3brKkozkLKciaK6t2j4zIb92n3kr20wckx-lbryR_uT9HPtvQukl0e2YAttU1obA
     // Key 24-1: sk-xesyREaXTPWzUOMW8r2pXQ:DlCw7YeqZHgfZy09CNQp1-p1g_xLPzIyzp55oNAunoC3bHTu2Qp5FN1q2SEDaemq9fHxRCTNdznwM1q0Zo39gw
@@ -12,4 +12,5 @@ public class BotConfig {
     // Key 24-3: sk-EOhqbJaSQnqF6GpUpnbLIQ:takmnWV6dnLiohsDO49Ou-etYP-N9B5o5LkiMc42po9mc7APUUTMfgdj8PbWuxZFjK1K6Y1YlL3v0uLoKYjo0Q
     // Key 24-4: sk-Xp9wLc8xSjKqNgi2US8eBw:chJ2G4941Lu6yzqpLy9ZtBleGked7woMeNRi0_xojkil1SovNYdFOKN-55eOPhbXTYeQNLlFrgXJJNDovuqeDQ
     // Key 24-5: sk-GmSmf-n2RjOVfQz74iUuHg:ZpxubQV5Vx2Uu3P7B1bbr0lpZSTlihOuGuKb0N7Cr_ZgGsMTV08nATYWUDixPTakbIT4P2jJbjLBDJR1FyNMdQ
+    // server: http://192.168.2.2:3000, https://cf25-server.jsclub.dev
 }
